ab619f20de74b2fff11a44237f198cf36b5574e3
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2024-01-07 22:27:44 -0300

    Feat: Homepage
    Homepage layout

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { Container } from './HomepageStyle';
 import Header from '../../components/Header';
+import MovieCard from '../../components/MovieCard';
+import { MoviesContainer } from './HomepageStyle';
 
 export default function Homepage() {
     return (
         <>
             <Container>
                 <Header/>
-                
+                <MoviesContainer>
+                    <MovieCard/>
+                    <MovieCard/>
+                    <MovieCard/>
+                    <MovieCard/>
+                    <MovieCard/>
+                    <MovieCard/>
+                    <MovieCard/>
+                    <MovieCard/>
+                    <MovieCard/>
+
+                </MoviesContainer>
+
             </Container>
         </>
     )
